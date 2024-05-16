3914e00c87ce959ffc62820d5014739891e0804c
isaac mwesigwa <mwesigwai433@gmail.com>
2024-05-16 13:47:12 +0300

    testauth edit

--- Hubtel.eCommerce.Cart.Api/Authentication/TestAuth.cs
+++ Hubtel.eCommerce.Cart.Api/Authentication/TestAuth.cs
@@ -22,22 +22,33 @@ namespace Hubtel.eCommerce.Cart.Api.Authentication
         {
             if (!Request.Headers.ContainsKey("Authorization"))
                 return AuthenticateResult.Fail("Unauthorized");
+            
             string authHeader = Request.Headers.Authorization!;
+            
             if (string.IsNullOrEmpty(authHeader))
                 return AuthenticateResult.Fail("Unauthorized");
+            
             if (!authHeader.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase))
                 return AuthenticateResult.Fail("Unauthorized");
+            
             var token = authHeader[6..];
             var credentialString = Encoding.UTF8.GetString(Convert.FromBase64String(token));
             var credentials = credentialString.Split(":");
+            
             if (credentials.Length != 2)
                 return AuthenticateResult.Fail("Unauthorized");
+            
             var username = credentials[0];
             var password = credentials[1];
+            
+            //adding users from here for test purposes only
             _helpers.AddUser("isaac", "4526","Admin");
             var user = _helpers.GetUser(username);
+            
             if (!username.Equals(user?.Username) && !password.Equals(user?.Password))
                 return AuthenticateResult.Fail("Unauthorized");
+            
+            
             var claims = new[]
             {
                 new Claim(ClaimTypes.Name,username),
@@ -45,6 +56,8 @@ namespace Hubtel.eCommerce.Cart.Api.Authentication
             };
             var claimsIdentity = new ClaimsIdentity(claims, "Basic");
             var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+           
+            
             return AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name));
 
         }
